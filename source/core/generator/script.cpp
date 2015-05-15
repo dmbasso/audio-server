38b7aee39563d46a959f19cefbb5f43446833cda
ccg <ccg@lvp-node1>
2015-05-15 18:52:11 +0100

    script generator implementation (wip)

--- source/core/generator/script.cpp
+++ source/core/generator/script.cpp
@@ -1,13 +1,97 @@
 #include "script.h"
+#include "../core.h"
 
 using namespace std;
 
 namespace aserver {
 namespace generator {
 
+Script::Script(Core *core, unsigned periodSize) : Wave(core, periodSize)
+{
+    ScriptConfigData *cfgData = new ScriptConfigData(); // set flags for all data in the constructor
+    cfgData->flags = scriptConfigFlags::SCRIPT_ALL ; // configure all data fields in constructor
+    config(cfgData);
+    counter = 0;
+}
+
+void Script::config(const ConfigData *configData)
+{
+    ScriptConfigData *cfgData = (ScriptConfigData *) configData;
+
+    if (cfgData->flags & scriptConfigFlags::PLAYBACK_STATE) {
+        playbackState = cfgData->playbackState;
+    }
+}
+
+void Script::addKeyframe(const Keyframe kf)
+{
+    keyframes[kf.start] = kf;
+}
 
 void Script::render ()
 {
+    switch (playbackState) {
+        case generator::playbackState::PLAYING : {
+
+            //manage previous state
+            if (playbackState == generator::playbackState::STOPPED) {
+                counter = 0;
+            }
+
+            playbackState = generator::playbackState::PLAYING;
+
+            map<unsigned, Keyframe>::iterator keyframesIt;// = keyframes.begin();
+
+            for (unsigned i = 0; i < keyframes.size(); i++) {
+                if (keyframesIt->first >= counter && keyframesIt->first < counter + buffer->getPeriodSize()) {
+                    break;
+                }
+                else {
+                    keyframesIt++;
+                }
+            }
+
+            int16_t sams[2];
+
+            for (unsigned i = counter; i < buffer->getPeriodSize() + counter; i++) {
+                if (i == keyframesIt->first) {
+                    //load keyframe info:
+                    wave = core->getWave(keyframesIt->second.filename);
+                    increment = keyframesIt->second.noteRatio;
+                    //load source(generator) position from keyframe
+
+                    position = 0; //start wave at the beginning
+                    keyframesIt++; // go to the next keyframe
+                }
+                //Same as Wave::render();
+                if (position == wave->getPeriodSize()) { //currently looping all waves
+                    position = 0;
+                }
+                buffer->writeFrame(wave->readFrame(sams, position), i);
+            }
+            counter += buffer->getPeriodSize();
+            break;
+        }
+        case generator::playbackState::STOPPED : {
+
+            //manage previous state
+            if (playbackState == generator::playbackState::PLAYING) {
+                counter = 0;
+            }
+
+            playbackState = generator::playbackState::STOPPED;
+
+            break;
+        }
+        case generator::playbackState::PAUSED : {
+
+            break;
+        }
+        case generator::playbackState::REWINDING : {
+
+            break;
+        }
+    }
 
 }
 
--- source/core/generator/script.h
+++ source/core/generator/script.h
@@ -1,7 +1,6 @@
-#ifndef GENERATOR_SCRIPT_H
-#define GENERATOR_SCRIPT_H
+#ifndef SCRIPT_H
+#define SCRIPT_H
 
-#include "generator.h"
 #include "wave.h"
 
 using namespace std;
@@ -9,11 +8,42 @@ using namespace std;
 namespace aserver {
 namespace generator {
 
+enum keyframeConfigFlags : uint64_t {
+    START =             0x1,
+    KEYFRAME_FILENAME = 0x2,
+    KEYFRAME_POSITION = 0X4,
+    NOTE_RATIO =        0X8,
+    KEYFRAME_ALL =      0XF
+};
+
+struct Keyframe {
+    int flags;
+    unsigned start;
+    const char *filename;
+    float position[3] = {0,0,0};
+    float noteRatio;
+};
+
+enum scriptConfigFlags : uint64_t {
+    PLAYBACK_STATE =    0x1,
+    SCRIPT_ALL =        0x1
+};
+
+struct ScriptConfigData : WaveConfigData {
+    generator::playbackState playbackState = generator::playbackState::STOPPED;
+};
+
+
 class Script :public Wave {
-private:
-    //keyframe and other info
-public:
-    virtual void render() override;
+    private:
+        map<unsigned, Keyframe> keyframes;
+        generator::playbackState playbackState;
+        unsigned counter;
+    public:
+        Script(Core *core, unsigned periodSize);
+        void config(const ConfigData *configData) override;
+        void addKeyframe(const Keyframe kf);
+        void render() override;
 };
 
 } //end generator namespace
