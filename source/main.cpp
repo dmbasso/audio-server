cb2a1bb473136f3329a221985d2125388c63e176
nunocs <nunomiguel06@gmail.com>
2015-07-02 16:33:11 +0100

    clean up, minor changes

--- source/main.cpp
+++ source/main.cpp
@@ -7,73 +7,11 @@
  */
 
 #include "core/core.h"
-#include "temp.h"
 
 using namespace aserver;
 using namespace std;
 
 Core core;
-
-// opera data
-generator::WaveConfigData celloWaveData, clarinetWaveData, double_bassWaveData, fluteWaveData, harpWaveData, oboeWaveData, percussionWaveData, tromboneWaveData, violinWaveData;
-processor::SourceConfigData celloSourceData, clarinetSourceData, double_bassSourceData, fluteSourceData, harpSourceData, oboeSourceData, percussionSourceData,tromboneSourceData, violinSourceData;
-
-void loadOperaData()
-{
-    celloWaveData.flags = generator::waveConfigFlags::WAVE_ALL;
-    celloWaveData.waveIndex = core.getWaveIndex("audio/input/cello.wav");
-    celloSourceData.loc = Location(2., 4., 0.82);
-    core.addGenerator(generator::types::WAVE, &celloWaveData);
-    core.addSource(&celloSourceData);
-
-    clarinetWaveData.flags = generator::waveConfigFlags::WAVE_ALL;
-    clarinetWaveData.waveIndex = core.getWaveIndex("audio/input/clarinet.wav");
-    clarinetSourceData.loc = Location(-1.45, 6., 0.8);
-    core.addGenerator(generator::types::WAVE, &clarinetWaveData);
-    core.addSource(&clarinetSourceData);
-
-    double_bassWaveData.flags = generator::waveConfigFlags::WAVE_ALL;
-    double_bassWaveData.waveIndex = core.getWaveIndex("audio/input/double_bass.wav");
-    double_bassSourceData.loc = Location(4.13, 4., 0.86);
-    core.addGenerator(generator::types::WAVE, &double_bassWaveData);
-    core.addSource(&double_bassSourceData);
-
-    fluteWaveData.flags = generator::waveConfigFlags::WAVE_ALL;
-    fluteWaveData.waveIndex = core.getWaveIndex("audio/input/flute.wav");
-    fluteSourceData.loc = Location(0., 6., 0.95);
-    core.addGenerator(generator::types::WAVE, &fluteWaveData);
-    core.addSource(&fluteSourceData);
-
-    harpWaveData.flags = generator::waveConfigFlags::WAVE_ALL;
-    harpWaveData.waveIndex = core.getWaveIndex("audio/input/harp.wav");
-    harpSourceData.loc = Location(-3.7, 4., 1.08);
-    core.addGenerator(generator::types::WAVE, &fluteWaveData);
-    core.addSource(&fluteSourceData);
-
-    oboeWaveData.flags = generator::waveConfigFlags::WAVE_ALL;
-    oboeWaveData.waveIndex = core.getWaveIndex("audio/input/oboe.wav");
-    oboeSourceData.loc = Location(2.13, 6., 0.63);
-    core.addGenerator(generator::types::WAVE, &oboeWaveData);
-    core.addSource(&oboeSourceData);
-
-    percussionWaveData.flags = generator::waveConfigFlags::WAVE_ALL;
-    percussionWaveData.waveIndex = core.getWaveIndex("audio/input/percussion.wav");
-    percussionSourceData.loc = Location(-3.5, 6., 0.64);
-    core.addGenerator(generator::types::WAVE, &percussionWaveData);
-    core.addSource(&percussionSourceData);
-
-    tromboneWaveData.flags = generator::waveConfigFlags::WAVE_ALL;
-    tromboneWaveData.waveIndex = core.getWaveIndex("audio/input/trombone.wav");
-    tromboneSourceData.loc = Location(4.2, 6., 1.23);
-    core.addGenerator(generator::types::WAVE, &tromboneWaveData);
-    core.addSource(&tromboneSourceData);
-
-    violinWaveData.flags = generator::waveConfigFlags::WAVE_ALL;
-    violinWaveData.waveIndex = core.getWaveIndex("audio/input/violin.wav");
-    violinSourceData.loc = Location(-1.33, 4., .85);
-    core.addGenerator(generator::types::WAVE, &violinWaveData);
-    core.addSource(&violinSourceData);
-}
 
 int main () {
 
@@ -95,17 +33,17 @@ int main () {
 
     //********** Acousticave processor example
     #ifdef WITH_AAVE
-    processor::AcousticaveConfigData aaveConfigData;
-    aaveConfigData.flags = processor::acousticaveConfigFlags::ACOUSTICAVE_ALL;
-    aaveConfigData.gain = 10;
-    aaveConfigData.hrtf = 1;
-    aaveConfigData.modelFilePath = "geometries/model.obj";
-    aaveConfigData.reflections = 0;
-    aaveConfigData.reverbActive = 1;
-    aaveConfigData.area = 3000;
-    aaveConfigData.volume = 3000;
-    aaveConfigData.rt60 = 4000;
-    core.setProcessor(processor::types::ACOUSTICAVE, &aaveConfigData);
+//    processor::AcousticaveConfigData aaveConfigData;
+//    aaveConfigData.flags = processor::acousticaveConfigFlags::ACOUSTICAVE_ALL;
+//    aaveConfigData.gain = 10;
+//    aaveConfigData.hrtf = 1;
+//    aaveConfigData.modelFilePath = "geometries/model.obj";
+//    aaveConfigData.reflections = 0;
+//    aaveConfigData.reverbActive = 1;
+//    aaveConfigData.area = 3000;
+//    aaveConfigData.volume = 3000;
+//    aaveConfigData.rt60 = 4000;
+    core.setProcessor(processor::types::ACOUSTICAVE);//, &aaveConfigData);
     #endif
 
     //********** Memory output example
@@ -158,8 +96,6 @@ int main () {
     //********** Script generator example
     core.addGenerator(generator::types::SCRIPT);
     core.addSource();
-
-//    loadOperaData();
 
     for (int i = 0; i < 1000; i++) {
 
